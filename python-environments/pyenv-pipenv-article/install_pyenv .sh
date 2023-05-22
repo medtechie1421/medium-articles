df194669640c62989994114f4802c6be36a1948f
medtechie1421 <134258260+medtechie1421@users.noreply.github.com>
2023-05-22 11:33:28 +0530

    Added install.sh for pyenv installation

---
+++ python-environments/pyenv-pipenv-article/install_pyenv .sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev \
+libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl 
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+
+exec bash
